7bd5bbc976a3cedb5efc5d5cf5251402081b2b52
loicgasser <loicgasser4@gmail.com>
2017-03-20 14:31:59 +0100

    Add permalink support

--- vib/static/olmap.js
+++ vib/static/olmap.js
@@ -1,4 +1,48 @@
 (function() {
+  var qString;
+  var dftX = 902568.527041534;
+  var dftY = 5969980.33812711;
+  var dftZoom = 10;
+
+  function getParam(name) {
+    return decodeURIComponent(
+        (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) ||
+            [,""])[1].replace(/\+/g, '%20')) || null;
+  }
+
+  function getParams() {
+    var x = getParam('x');
+    var y = getParay('y');
+    var zoom = getParam('zoom');
+    return {
+      x: x || dftX,
+      y: y || dftY,
+      zoom: zoom || dftZoom
+    };
+  }
+
+  function setParams(opts) {
+    if (!qString) {
+      qString = $.query.set('x', opts.x);
+    } else {
+      qString = qString.set('x', opts.x);
+    }
+    qString = qString.set('y', opts.y);
+    qString = qString.set('zoom', opts.zoom);
+    history.pushState({}, '', window.location.pathname + qString.toString());
+  }
+
+  function permalinkManager(map) {
+    return map.on('moveend', function() {
+      var zoom = map.getView().getZoom();
+      var center = map.getView().getCenter();
+      setParams({
+        x: center[0],
+        y: center[1],
+        zoom: zoom
+      });
+    });
+  }
 
   function initMap() {
     var key = 'pk.eyJ1IjoidmliMmQiLCJhIjoiY2l5eTlqcGtoMDAwZzJ3cG56emF6YmRoOCJ9.lP3KfJVHrUHp7DXIQrZYMw';
@@ -17,7 +61,9 @@
     });
     map.getLayers().setAt(0, wmtsLayer);
     var view = map.getView();
-    view.setCenter([902568.527041534, 5969980.33812711]);
+    view.setCenter([dftX, dftY]);
+    view.setZoom(dftZoom);
+    permalinkManager(map);
   }
 
   $(window).load(function() {
