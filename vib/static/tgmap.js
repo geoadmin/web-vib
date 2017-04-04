e82ba733066f26290604198489643a651527c922
oterral <olivier.terral@swisstopo.ch>
2017-04-04 16:36:58 +0200

    Add click event on tangram

--- vib/static/tgmap.js
+++ vib/static/tgmap.js
@@ -1,6 +1,6 @@
 var params = getParams();
+var info = $('#info');
 var map = L.map('map');
-
 var layer = Tangram.leafletLayer({
   scene: params.scene
 });
@@ -17,5 +17,55 @@ map.on('moveend', function() {
   $('#mapzenLink')[0].href = 'https://mapzen.com/tangram/play/?scene=' + 
       encodeURIComponent(scene) +
       '#' + zoom + '/' + center.lat + '/' + center.lng;
+});
+
+// PICKING
+map.getContainer().addEventListener('mousemove', function (event) {
+  var pixel = {
+    x: event.clientX,
+    y: event.clientY
+  };
+
+  layer.scene.getFeatureAt(pixel).then(function(selection) {
+    map.getContainer().style.cursor = (selection && selection.feature)? 'pointer' : 'inherit';
+  });
+})
+ 
+map.getContainer().addEventListener('click', function (event) {
+  var pixel = {
+    x: event.clientX,
+    y: event.clientY
+  };
+  info.hide();
+
+  layer.scene.getFeatureAt(pixel).then(function(selection) {
+    if (!selection || ! selection.feature) {
+      return;
+    }
+    var feat = selection.feature;
+    var label = '';
+    if (feat.properties) {
+      var sorted = [];
+      var count = 0;
+      Object.keys(feat.properties).sort().forEach(function(v, i) {
+        sorted.push([v, feat.properties[v]]);
+        count++;
+      });
+      label += "layer : " + feat.layers + "<br>";
+      for (x in sorted) {
+        key = sorted[x][0];
+        val = sorted[x][1];
+        label += "<div class='labelLine' key='" + key + "' value='" + val + "' onclick='setValuesFromSpan(this)'>" + key + " : " + val + "</div>";
+      }
+    }
+    if (label != '') {
+      info.css({
+        left: (pixel.x + 5) + 'px',
+        top: (pixel.y + 15) + 'px'
+      });
+      info[0].innerHTML = '<span class="labelInner">' + label + '</span>';
+      info.show();
+    }
+  });
 });
 
