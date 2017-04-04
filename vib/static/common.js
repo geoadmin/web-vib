c9bee7b9ce777c76f21a5e1c939b1776fc692874
oterral <olivier.terral@swisstopo.ch>
2017-04-04 10:47:14 +0200

    Use common js to manage paramters

---
+++ vib/static/common.js
@@ -0,0 +1,71 @@
+var qString;
+var dftLng = 8.25381975151518;
+var dftLat = 46.77713656930146;
+var dftX = 902568.527041534;
+var dftY = 5969980.33812711;
+var dftZoom = 8;
+var dftBackground = 'swissimage';
+var dftLang = 'all';
+
+// Mapboxgl
+var dftStyle = 'cj12a02uq007z2rqu53z4en4o';
+
+// TangramJS
+var dfltScene = 'https://mapzen.com/api/scenes/46449/455/resources/basic.yaml'; 
+ 
+function getParam(name) {
+  return decodeURIComponent(
+      (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) ||
+          [,""])[1].replace(/\+/g, '%20')) || null;
+}
+
+function getParams() {
+  var lng = getParam('lng');
+  var lat = getParam('lat');
+  var x = getParam('x');
+  var y = getParam('y');
+  var zoom = getParam('zoom');
+  var style = getParam('style');
+  var lang = getParam('lang');
+  var background = getParam('background');
+  var scene = getParam('scene');
+  return {
+    lng: lng || dftLng,
+    lat: lat || dftLat,
+    zoom: zoom || dftZoom,
+    lang: lang || dftLang,
+    x: x || dftX,
+    y: y || dftY,
+    style: style || dftStyle,
+    lang: lang || dftLang,
+    background: background || dftBackground,
+    scene: scene || dfltScene
+  };
+}
+
+function setParam(value, key) {
+  if (!qString) {
+    qString = $.query.set(key, value);
+  } else {
+    qString = qString.set(key, value);
+  }
+  history.pushState({}, '', window.location.pathname + qString.toString());
+  params = getParams();
+}
+
+function setParams(opts) {
+  if (!qString) {
+    qString = $.query.set('lng', opts.lng);
+  } else {
+    qString = qString.set('lng', opts.lng);
+  }
+  qString = qString.set('lat', opts.lat);
+  qString = qString.set('x', opts.x);
+  qString = qString.set('y', opts.y);
+  qString = qString.set('zoom', opts.zoom);
+  qString = qString.set('style', opts.style);
+  qString = qString.set('lang', opts.lang);
+  qString = qString.set('background', opts.background);
+  history.pushState({}, '', window.location.pathname + qString.toString());
+}
+
--- vib/static/tgmap.js
+++ vib/static/tgmap.js
@@ -1,8 +1,21 @@
+var params = getParams();
 var map = L.map('map');
 
-// Mpzen Play link: https://mapzen.com/tangram/play/?scene=https%3A%2F%2Fmapzen.com%2Fapi%2Fscenes%2F46449%2F453%2Fresources%2Fbasic.yaml#7.583/46.596/8.735
 var layer = Tangram.leafletLayer({
-  scene: 'https://mapzen.com/api/scenes/46449/453/resources/basic.yaml'
+  scene: params.scene
 });
 layer.addTo(map);
-map.setView([46.25, 7.94], 8);
+map.setView([params.lat, params.lng], params.zoom);
+map.on('moveend', function() {
+  var center = map.getCenter();
+  var zoom = map.getZoom();
+  var scene = layer.scene.config_source;
+  setParam(center.lat, 'lat');
+  setParam(center.lng, 'lng');
+  setParam(zoom, 'zoom');
+  setParam(scene, 'scene');
+  $('#mapzenLink')[0].href = 'https://mapzen.com/tangram/play/?scene=' + 
+      encodeURIComponent(scene) +
+      '#' + zoom + '/' + center.lat + '/' + center.lng;
+});
+
