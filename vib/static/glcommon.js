83b8647930376648d110d2ae8f7aaef3fe2e27a5
loicgasser <loicgasser4@gmail.com>
2017-05-22 16:49:48 +0200

    Enable sub categories selection

--- vib/static/glcommon.js
+++ vib/static/glcommon.js
@@ -1,7 +1,6 @@
 var glapi = {};
 
 (function() {
-
   glapi.accessToken = 'pk.eyJ1IjoidmliMmQiLCJhIjoiY2l5eTlqcGtoMDAwZzJ3cG56' +
       'emF6YmRoOCJ9.lP3KfJVHrUHp7DXIQrZYMw';
 
@@ -13,16 +12,17 @@ var glapi = {};
   glapi.getWMTSUrl = function(bodId, timestamp, format) {
     format = format ? format : 'png';
     timestamp = timestamp ? timestamp : 'current';
-    var templateUrl = 'https://wmts10.geo.admin.ch/1.0.0/{bodId}/default/{timestamp}/' +
-          '3857/{z}/{x}/{y}.{format}'
+    var templateUrl = 'https://wmts10.geo.admin.ch/1.0.0/{bodId}/default/'
+        '{timestamp}/3857/{z}/{x}/{y}.{format}'
     return templateUrl.replace('{bodId}', bodId)
           .replace('{timestamp}', timestamp)
           .replace('{format}', format);
   };
 
   glapi.getWMSUrl = function(bodId) {
-    return 'https://wms.geo.admin.ch?bbox={bbox-epsg-3857}&format=image/png&STYLES=&' +
-        '&service=WMS&version=1.3.0&request=GetMap&crs=EPSG:3857&width=256&height=256&layers=' + bodId;
+    return 'https://wms.geo.admin.ch?bbox={bbox-epsg-3857}&format=image/png&' +
+        'STYLES=&&service=WMS&version=1.3.0&request=GetMap&crs=EPSG:3857&' +
+        'width=256&height=256&layers=' + bodId;
   };
 
   glapi.formatSourceName = function(bodId) {
@@ -103,40 +103,64 @@ var glapi = {};
       }
     };
 
-    this.addLayerGroup = function(layerGroupId, beforeLayerId) {
-      var that, group, groupOptions, sourceId, addLayerSet, getLayerSet, updateGeoJSONSource;
-      that = this;
-      const props = ['minzoom', 'maxzoom', 'layout', 'filter', 'type',
-          'source', 'paint', 'metadata', 'source-layer', 'sourceLayer'];
-
-      addLayerSet = function(data) {
-        var ref, extended, layers, layer, prop;
-        layers = data.layers;
-        for (var i=0; i < layers.length; i++) {
-          layer = layers[i];
-          // We use the metadata field to keep track of the groupId
-          layer.metadata = {groupId: layerGroupId};
-          // Ideally we should get rid of ref alltogether
-          if (layer.ref) {
-            extended = {};
-            ref = that.map.getLayer(layer.ref);
-            for (var j in props) {
-              prop = props[j];
-              // user defined ref are no longer supported
-              if (layer[prop]) {
-                extended[prop] = layer[prop];
-              } else if (ref[prop]) {
-                propExt = prop == 'sourceLayer' ? 'source-layer' : prop;
-                extended[propExt] = ref[prop];
-              }
-            }
-            extended.id = layer.id;
-            that.map.addLayer(extended, beforeLayerId);
-          } else {
-            that.map.addLayer(layers[i], beforeLayerId);
+    this.getLayerGroupAlternativesById = function(layerGroupId) {
+      var layerGroup, layerGroupOptions, alternatives;
+      alternatives = [];
+      layerGroup = this.getLayerGroupById(layerGroupId);
+      for (var i=0; i < layerGroup.sources.length; i++) {
+        if (layerGroup.alternatives[i]) {
+          var source = layerGroup.sources[i];
+          var layerset = layerGroup.layerset[i];
+          layerGroupOptions = this.getLayerGroupOptionsBySourceId(source);
+          if (layerGroupOptions.layersetLabels &&
+              layerGroupOptions.layersetLabels.length) {
+            alternatives = alternatives.concat(
+                layerGroupOptions.layerset.map(function(e, i) {
+              return [e, layerGroupOptions.layersetLabels[i]];
+              })
+            );
           }
         }
-      };
+      }
+      return alternatives;
+    };
+
+    this.addLayer = function(layer, layerGroupId, beforeLayerId) {
+      var extended, prop, propExt;
+      const props = ['minzoom', 'maxzoom', 'layout', 'filter', 'type',
+          'source', 'paint', 'metadata', 'source-layer', 'sourceLayer'];
+      layer.metadata = {groupId: layerGroupId};
+      if (layer.ref) {
+        extended = {};
+        ref = this.map.getLayer(layer.ref);
+        for (var j in props) {
+          prop = props[j];
+          // user defined ref are no longer supported
+           if (layer[prop]) {
+            extended[prop] = layer[prop];
+           } else if (ref[prop]) {
+            propExt = prop == 'sourceLayer' ? 'source-layer' : prop;
+            extended[propExt] = ref[prop];
+          }
+        }
+        extended.id = layer.id;
+        this.map.addLayer(extended, beforeLayerId);
+      } else {
+        this.map.addLayer(layer, beforeLayerId);
+      }
+    };
+
+    this.addLayerset = function(layers, layerGroupId, beforeLayerId) {
+      var layer;
+      for (var i=0; i < layers.length; i++) {
+        layer = layers[i];
+        this.addLayer(layer, layerGroupId, beforeLayerId);
+      }
+    };
+
+    this.addLayerGroup = function(layerGroupId, layerSetId, beforeLayerId) {
+      var that, group, groupOptions, sourceId, updateGeoJSONSource;
+      that = this;
       updateGeoJSONSource = function(sourceId) {
         glapi.getGeoJSON(sourceId).then(function(data) {
           that.map.getSource(sourceId).setData(data);
@@ -147,8 +171,13 @@ var glapi = {};
       group = this.getLayerGroupById(layerGroupId);
       for (var i=0; i < group.sources.length; i++) {
         sourceId = group.sources[i];
-        layersetId = group.layerset[i];
         groupOptions = this.getLayerGroupOptionsBySourceId(sourceId);
+        // Select correct layerset (only one per layer)
+        if (layerSetId && groupOptions.layerset.indexOf(layerSetId) != -1) {
+          layersetId = layerSetId;
+        } else {
+          layersetId = group.layerset[i];
+        }
         layerSets.push(glapi.getLayerset(sourceId, layersetId));
         if (groupOptions.type == 'geojson') {
           updateGeoJSONSource(sourceId);
@@ -157,13 +186,38 @@ var glapi = {};
       $.when.all(layerSets).then(function(layersets) {
         for (var k=0; k < layersets.length; k++) {
           if (layersets[k][0]) {
-            addLayerSet(layersets[k][0]);
+            that.addLayerset(layersets[k][0].layers, layerGroupId, beforeLayerId);
           } else {
             // When we only have one source
-            addLayerSet(layersets[k]);
+            that.addLayerset(layersets[k].layers, layerGroupId, beforeLayerId);
             break;
           }
         }
+      });
+    };
+
+    this.updateLayerset = function(layerGroupId, layersetId) {
+      var that, style, group, sourceId;
+      that = this;
+      style = this.map.getStyle();
+      group = this.getLayerGroupById(layerGroupId);
+      sourceId = group.sources[group.alternatives.indexOf(true)];
+      glapi.getLayerset(sourceId, layersetId).then(function(data) {
+        var beforLayerId;
+        for (var i=0; i < style.layers.length; i++) {
+          var groupId = style.layers[i].metadata.groupId;
+          var srcId = style.layers[i].source;
+          if (layerGroupId == groupId && srcId == sourceId) {
+            var layerId = style.layers[i].id;
+            that.map.removeLayer(layerId);
+            if (i + 1 == style.layers.length) {
+              beforeLayerId = undefined;
+            } else if (i < style.layers.length) {
+              beforeLayerId = style.layers[i].id;
+            }
+          }
+        }
+        that.addLayerset(data.layers, layerGroupId, beforeLayerId);
       });
     };
 
--- vib/static/glgroupapi.js
+++ vib/static/glgroupapi.js
@@ -1,7 +1,8 @@
-(function() {
+var ga = {};
 
+(function() {
   function initCarousel(groups) {
-    var layers, layer, i, previousLayerGroupId;
+    var layers, layer, alts, altBtns, i, previousLayerGroupId;
     i = 0;
     layers = groups.getAllLayerGroups();
     for(var key in layers) {
@@ -9,7 +10,30 @@
         previousLayerGroupId = key;
       }
       layer = layers[key];
-      $('<div id="'+ key +'" class="item">' + layer.label + '</div></div>').appendTo('.carousel-inner');
+      altsBtn = '';
+      alts = groups.getLayerGroupAlternativesById(key);
+      if (alts && alts.length) {
+        altsBtn = '<br>';
+        for (var k=0; k < alts.length; k++) {
+          var selectedLayerset = '';
+          var layerset = alts[k][0];
+          var layersetLabel = alts[k][1];
+          if (layer.layerset.indexOf(layerset) != -1) {
+            selectedLayerset = layerset;
+          }
+          altsBtn += '<button class="btn btn-sm ';
+          if (selectedLayerset) {
+            altsBtn += 'active" ';
+          } else {
+            altsBtn += '" ';
+          }
+          altsBtn += 'layerset="' + layerset + '" onclick="ga.toggleLayerset(\'' +
+              key + '\', \'' + layerset + '\')">' + layersetLabel + '</button>';
+        }
+      }
+      $('<div id="' + key.replace(/\./g, '_') + '" group="' + key +
+          '" class="item">' + layer.label + altsBtn +
+          '</div></div>').appendTo('.carousel-inner');
       $('<li data-target="#layersCarousel" data-slide-to="'+ i +'"></li>')
           .appendTo('.carousel-indicators');
       i += 1;
@@ -17,15 +41,16 @@
     $('.item').first().addClass('active');
     $('.carousel-indicators > li').first().addClass('active');
     $('#layersCarousel').bind('slide.bs.carousel', function (e) {
-      var layerGroupId = e.relatedTarget.id;
+      var layerGroupId = $(e.relatedTarget).attr('group');
+      var layersetId = $(e.relatedTarget).find('button.active').attr('layerset');
       groups.removeLayerGroup(previousLayerGroupId);
-      groups.addLayerGroup(layerGroupId);
+      groups.addLayerGroup(layerGroupId, layersetId);
       previousLayerGroupId = layerGroupId;
     });
   }
 
   function initMap() {
-    var map, backgroundLayerId;
+    var map, backgroundLayerId, groups;
     backgroundLayerId = 'ch.swisstopo.lightbasemap';
     mapboxgl.accessToken = glapi.accessToken;
     app.getParams();
@@ -39,9 +64,15 @@
       minZoom: 6,
       maxZoom: 18
     });
+    groups = new glapi.MapLayerGroups(map);
+    // Ui controllers
+    ga.toggleLayerset = function(layerGroupId, layersetId) {
+      groups.updateLayerset(layerGroupId, layersetId);
+      $('#' + layerGroupId.replace(/\./g, '_') + ' button.active').removeClass('active');
+      $(event.target).addClass('active');
+    };
 
     map.on('load', function(e) {
-      var groups = new glapi.MapLayerGroups(map);
       groups.onReady(function() {
         initCarousel(groups);
         groups.addLayerGroup(backgroundLayerId);
